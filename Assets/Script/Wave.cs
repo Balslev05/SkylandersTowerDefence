cfbbf1f80b3248c33f61014eb86ee74b9e3cc191
ShamiDud <samuelkkristensen@gmail.com>
2025-03-27 11:45:28 +0100

    WaveSystemBase

---
+++ Assets/Script/Wave.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "WaveSystem", order = 1)]
+public class Wave : ScriptableObject
+{
+    public List<GameObject> Enemies = new List<GameObject>();
+
+    public List<bool> SpawnLeft = new List<bool>();
+
+    [HideInInspector] public float spawnDelay = 0.1f;
+}
---
+++ Assets/Script/WaveManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] private WayPointManager LeftWayPointManager;
+    [SerializeField] private WayPointManager RightWayPointManager;
+
+    [SerializeField] private List<Wave> waves = new List<Wave>();
+
+    private int currentWave;
+
+    private void Start()
+    {
+        currentWave = 0;
+        StartCoroutine(StartBattle());
+    }
+
+    private void Spawn(WayPointManager SpawnPoint, GameObject enemy)
+    {
+        EnemyBase spawnedEnemy = Instantiate(enemy, SpawnPoint.spawnPoint, Quaternion.identity).GetComponent<EnemyBase>();
+        spawnedEnemy.wayPointManager = SpawnPoint;
+        spawnedEnemy.target = SpawnPoint.wayPoints[0];
+    }
+
+    private IEnumerator StartBattle()
+    {
+        for (int i = 0; i < waves[currentWave].Enemies.Count; i++)
+        {
+            if (waves[currentWave].SpawnLeft[i])
+            {
+                Spawn(LeftWayPointManager, waves[currentWave].Enemies[i]);
+            }
+            else
+            {
+                Spawn(RightWayPointManager, waves[currentWave].Enemies[i]);
+            }
+
+            yield return new WaitForSeconds(waves[currentWave].spawnDelay);
+        }
+    }
+
+    private bool CheckEnemiesLeft()
+    {
+        return false;
+    }
+}
